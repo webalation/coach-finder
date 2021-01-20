bd8b11a75e98fe50ac0582ea878a1893bd00b1ea
Richard Ball <webalation@gmail.com>
2021-01-20 07:34:38 -0500

    Fetching coach data (GET Http request)

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -27,5 +27,31 @@ export default {
       ...coachData,
       id: userId
     });
+  },
+  async loadCoaches(context) {
+    const response = await fetch(
+      `https://coach-finder-a95b4-default-rtdb.firebaseio.com/coaches.json`
+    );
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      // ...
+    }
+
+    const coaches = [];
+
+    for (const key in responseData) {
+      const coach = {
+        id: key,
+        firstName: responseData[key].firstName,
+        lastName: responseData[key].lastName,
+        description: responseData[key].description,
+        hourlyRate: responseData[key].hourlyRate,
+        areas: responseData[key].areas
+      };
+      coaches.push(coach);
+    }
+
+    context.commit('setCoaches', coaches);
   }
 };
